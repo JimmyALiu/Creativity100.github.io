7e668e8a1d4b6b223b24cabd295d21acffed4f3b
Creativity100 <84637599+Creativity100@users.noreply.github.com>
2023-10-21 18:54:25 -0700

    Upload about me website

---
+++ script.js
@@ -0,0 +1,20 @@
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if(entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
+
+// rehiding elements
+const observeHead = new IntersectionObserver((entry) => {
+  console.log("head");
+  hiddenElements.forEach((el) => {
+    el.classList.remove("show");
+  })
+})
+const headerEl = document.querySelector("#top");
+observeHead.observe(headerEl)
